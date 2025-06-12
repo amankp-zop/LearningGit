63e601a586fa7f252fd1f1d069025641543d3eaf
amankp-zop <amankumar.Pandey@zop.dev>
2025-06-12 07:31:29 +0530

    Added the Assignment solution for 1st day assignment

---
+++ day1/assignment2.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var currencyMap = map[string]float64{
+	"USDINR": 85.00,
+	"USDUSD": 1,
+	"USDJPY": 145.00,
+	"USDEUR": 0.90,
+	"JPYUSD": 0.0069,
+	"JPYJPY": 1,
+	"JPYINR": 0.58,
+	"JPYEUR": 0.0062,
+	"INRUSD": 0.012,
+	"INRJPY": 1.72,
+	"INRINR": 1,
+	"INREUR": 0.011,
+	"EURUSD": 1.11,
+	"EURJPY": 161.00,
+	"EURINR": 91.00,
+	"EUREUR": 1,
+}
+
+var amount float64
+
+func isValidCurrency(curr string) bool {
+	return curr == "USD" || curr == "INR" || curr == "JPY" || curr == "EUR"
+}
+
+func validateInput(arr []string) bool {
+	if len(arr) != 3 {
+		return false
+	}
+	value, err := strconv.ParseFloat(arr[0], 64)
+	if err != nil {
+		fmt.Println("Invalid amount:", arr[0])
+		return false
+	}
+	amount = value
+	if !isValidCurrency(arr[1]) || !isValidCurrency(arr[2]) {
+		fmt.Println("Invalid currency code.")
+		return false
+	}
+
+	return true
+}
+
+func currencyConverter(amount float64, initialCurrency, finalCurrency string) float64 {
+	return amount * currencyMap[initialCurrency+finalCurrency]
+}
+
+func main() {
+
+	argsProg := os.Args
+	argsWithoutPath := os.Args[1:]
+
+	fmt.Println(argsProg)
+	fmt.Println(argsWithoutPath)
+
+	if validateInput(argsWithoutPath) {
+
+		convertedAmount := currencyConverter(amount, argsWithoutPath[1], argsWithoutPath[2])
+		fmt.Println(amount, argsWithoutPath[1], "is equal to", convertedAmount)
+
+	} else {
+		fmt.Println("The input needs to be in the following format: go run fileName Amount IntialCurrency ConvertedCurrency")
+	}
+
+}
