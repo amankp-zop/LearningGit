14a89ab1540149c77e7a34e4e65434452120bae3
amankp-zop <amankumar.Pandey@zop.dev>
2025-06-17 18:00:32 +0530

    Added the comments for the assignment code.

--- day5/Version1/main.go
+++ day5/Version1/main.go
@@ -5,43 +5,51 @@ import (
 	"math"
 )
 
+// Shape is an interface that defines a method to calculate area.
 type Shape interface {
 	Area() float64
 }
 
+// Rectangle represents a rectangle with length and breadth.
 type Rectangle struct {
 	length  float64
 	breadth float64
 }
 
+// Circle represents a circle with a given radius.
 type Circle struct {
 	radius float64
 }
 
+// Area calculates and returns the area of the circle.
 func (cir *Circle) Area() float64 {
 	return cir.radius * cir.radius * math.Pi
 }
 
+// Area calculates and returns the area of the rectangle.
 func (rec *Rectangle) Area() float64 {
 	return rec.breadth * rec.length
 }
 
+// findArea takes a Shape and returns its area.
 func findArea(s Shape) float64 {
 	return s.Area()
 }
 
 func main() {
-
+	// Create a Rectangle instance.
 	rec := Rectangle{
 		length:  10,
 		breadth: 10,
 	}
 
+	// Create a Circle instance.
 	cir := Circle{
 		radius: 20,
 	}
 
+	// Print the area of the circle.
 	fmt.Println("The Area of the circle is:", findArea(&cir))
+	// Print the area of the rectangle.
 	fmt.Println("The Area of the rectangle is:", findArea(&rec))
-
 }
--- day5/Version2/main.go
+++ day5/Version2/main.go
@@ -2,43 +2,54 @@ package main
 
 import "fmt"
 
+// Logger is an interface that defines a logging method.
 type Logger interface {
 	Log(message string)
 }
 
+// ConsoleLogger implements Logger for console output.
 type ConsoleLogger struct{}
+
+// FileLogger implements Logger for file output (simulated).
 type FileLogger struct{}
+
+// RemoteLogger implements Logger for remote output (simulated).
 type RemoteLogger struct{}
 
+// Log prints a message to the console for ConsoleLogger.
 func (logger *ConsoleLogger) Log(message string) {
 	fmt.Println("The message from the ConsoleLogger is: ", message)
 }
+
+// Log prints a message to the console for FileLogger (simulating file logging).
 func (logger *FileLogger) Log(message string) {
 	fmt.Println("The message from the FileLogger is: ", message)
 }
+
+// Log prints a message to the console for RemoteLogger (simulating remote logging).
 func (logger *RemoteLogger) Log(message string) {
 	fmt.Println("The message from the RemoteLogger is: ", message)
 }
 
+// logAll logs a message using all provided Logger implementations.
 func logAll(loggers []Logger, message string) {
-
 	for _, item := range loggers {
 		item.Log(message)
 	}
-
 }
 
 func main() {
-
+	// Create instances of each logger type.
 	cl := ConsoleLogger{}
 	fl := FileLogger{}
 	rl := RemoteLogger{}
 
+	// Create a slice of Logger interfaces and assign logger instances.
 	slice := make([]Logger, 3)
 	slice[0] = &cl
 	slice[1] = &fl
 	slice[2] = &rl
 
+	// Log a message using all loggers.
 	logAll(slice, "Not gonna lie, it was playing with my sanity.")
-
 }
